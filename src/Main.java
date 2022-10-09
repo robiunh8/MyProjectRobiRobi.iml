d04a0a4216b0a76e4051b5ed6bc2f0013ba19a1b
robiunh8 <robert.kerekes@yahoo.com>
2022-10-09 23:17:22 +0300

    tema tema QA29

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tema lab09 CURS QA29");
-        int op1 = -5 + 8 * 6 ;
-        int op2 = (55+9) % 9 ;
-        int op3 = 20 + -3*5 / 8 ;
-        int op4 = 5 + 15 / 3 * 2 - 8 % 3 ;
+        System.out.println("Hello") ;
+        System.out.println("Robi") ;
+
+        int op1 = 10+10 ;
+
+        int op2 = 30/5 ;
+
+        int op3 = -5 + 8 * 6 ;
+        int op4 = (55+9) % 9 ;
+        int op5 = 20 + -3*5 / 8 ;
+        int op6 = 5 + 15 / 3 * 2 - 8 % 3 ;
+
         System.out.println(op1);
         System.out.println(op2);
         System.out.println(op3);
         System.out.println(op4);
+        System.out.println(op5);
+        System.out.println(op6);
+
+
 
 
 
